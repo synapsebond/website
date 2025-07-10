4685fe1a9aaebd8bbad35f1ecd9d732e53a80715
HanzCEO <HanzCEO@users.noreply.github.com>
2025-07-10 14:50:45 +0000

    deploy: a20f29928db3141a7214049eb666317884a97b16

--- assets/chevron-left-Cp-paMO2.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-DjPn4H4n.js";import"./index-B2EvYR2C.js";import"./index.es-Ab3RR8A6.js";import"./events-DQ172AOg.js";const t=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M11.04 1.46a1 1 0 0 1 0 1.41L5.91 8l5.13 5.13a1 1 0 1 1-1.41 1.41L3.79 8.71a1 1 0 0 1 0-1.42l5.84-5.83a1 1 0 0 1 1.41 0Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{t as chevronLeftSvg};
